8fc6bb58a2703b9bd7570ebec50d860cc189ccc5
davidTradie <76200052+davidTradie@users.noreply.github.com>
2020-12-18 15:24:32 +0800

    Update credit_card_model.dart

--- lib/credit_card_model.dart
+++ lib/credit_card_model.dart
@@ -1,10 +1,11 @@
 class CreditCardModel {
 
-  CreditCardModel(this.cardNumber, this.expiryDate, this.cardHolderName, this.cvvCode, this.isCvvFocused);
+  CreditCardModel(this.cardNumber, this.expiryDate, this.cardHolderName, this.cvvCode, this.isCvvFocused,this.email);
 
   String cardNumber = '';
   String expiryDate = '';
   String cardHolderName = '';
   String cvvCode = '';
+  String email;
   bool isCvvFocused = false;
 }
